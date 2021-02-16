af191ff28e4dda404f99ca3a5fdc1bebe58cfebe
Guilherme Molina Trindade <78188699+Molina0127@users.noreply.github.com>
2021-02-16 15:00:27 -0300

    Atualização

--- Exercicio3.java
+++ Exercicio3.java
@@ -7,57 +7,7 @@ public class Exercicio3 {
 		Integer array2d[][] = new Integer[100][100];
 		Integer a, b = 0;
 		
-		for(a = 0; a < 100; a++) {
-			for(b = 0; b < 100; b++) {
-				array2d[a][b] = (a * 0)-(b * 0);
-				}
-			}
-		
-		
-		for(a = 0; a < 100; a++) {
-			for(b = 0; b < 100; b++) {
-				array2d[a][b] = (a * 7)-(b * 7);
-			
-			}
-			
-		}
-		
-		
-		for(a = 0; a < 100; a++) {
-			for(b = 0; b < 100; b++) {
-			
-				if((array2d[a][b] <= -100)){
-					System.out.print("|"+array2d[a][b]+" |");
-				}
-				
-				if((array2d[a][b] <= -10)&&(-99 <= array2d[a][b])){
-					System.out.print("| "+array2d[a][b]+" |");
-				}
-				
-				if((array2d[a][b] <= -1)&&(array2d[a][b] >= -9)) {
-					System.out.print("|  "+array2d[a][b]+" |");
-				}
-				
-				if((array2d[a][b] >= 0)&&(9 >= array2d[a][b])){
-					System.out.print("|  "+array2d[a][b]+"  |");
-				}
-				
-				if((array2d[a][b] >= 10)&&(99 >= array2d[a][b])){
-					System.out.print("|  "+array2d[a][b]+" |");
-				}
-				
-				if((array2d[a][b] >= 100)){
-					System.out.print("| "+array2d[a][b]+" |");
-				}
-				
-			}
-			
-			System.out.println();
-			
-		}
-		
-
-		System.out.println();
+//Talvez não seja possível visualizar todos os elementos por conta do tamanho do console
 		
 		for(a = 0; a < 100; a++) {
 			for(b = 0; b < 100; b++) {
@@ -85,9 +35,38 @@ public class Exercicio3 {
 		}
 
 		
+		System.out.println();
 		
 		
-
+		for(a = 0; a < 100; a++) {
+			for(b = 0; b < 100; b++) {
+				array2d[a][b] = (a*7)-(b*7);
+			}
+		}
+		
+		for(a = 0; a < 100; a++) {
+			for(b = 0; b < 100; b++) {
+			
+				
+				if((array2d[a][b] >= 100)||((array2d[a][b] >= -99)&&(array2d[a][b] <= -10))) {
+					System.out.print("| "+array2d[a][b]+" |");
+				}
+				
+				if((array2d[a][b] >= 0)&&(array2d[a][b] < 10)) {
+					System.out.print("|  "+array2d[a][b]+"  |");
+				}
+				if(((array2d[a][b] >= 10)&&(array2d[a][b] <= 99))||((array2d[a][b] <= -1)&&(array2d[a][b] >= -9))) {
+					System.out.print("|  "+array2d[a][b]+" |");
+				}
+				if(array2d[a][b] <= -100) {
+					System.out.print("| "+array2d[a][b]+"|");
+				}
+			
+			}
+			
+			System.out.println();
+			
+		}
 
 	}
 }
